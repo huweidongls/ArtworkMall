964da15d0b08d72d76aea5837ea3fd24c971dd09
huweidong <466463054@qq.com>
2019-12-10 14:55:03 +0800

    123

--- app/src/main/java/com/jingna/artworkmall/page/MainActivity.java
+++ app/src/main/java/com/jingna/artworkmall/page/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jingna.artworkmall;
+package com.jingna.artworkmall.page;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jingna.artworkmall.R;
 
 public class MainActivity extends AppCompatActivity {
 
